d69e0566faadbe9e25ff9280db4ed5923914402a
Oliver <molivera@8amarketing.com>
2021-04-08 19:59:47 -0300

    Update Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
     SET_LOADING,
     LOGS_ERROR,
     ADD_LOG,
-    DELETE_LOG
+    DELETE_LOG,
+    UPDATE_LOG,
+    SET_CURRENT,
+    CLEAR_CURRENT
 } from './types';
 
 // export const getLogs = () => {
@@ -85,8 +88,49 @@ export const deleteLog = (id) => async dispatch => {
             type: LOGS_ERROR,
             payload: err.response.data
         });
+    }    
+}
+
+// Update log from server
+export const updateLog = log => async dispatch => {   
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json();
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });    
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }    
+}
+
+// Set current log
+export const setCurrent = log => {    
+    return {
+        type: SET_CURRENT,
+        payload: log
     }
-    
+}
+
+// Clear Current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT
+    }
 }
 
 // Set loading to true 
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
 
     const { id, tech, attention, message, date } = log;
 
@@ -19,7 +19,7 @@ const LogItem = ({ log, deleteLog }) => {
     return (
         <li className='collection-item'>
             <div>
-                <a href="#edit-log-modal" className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}>{message}</a>
+                <a href="#edit-log-modal" onClick={() => setCurrent(log)} className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}>{message}</a>
                 <br/>
                 <span className='grey-text'>
                     <span className="black-text">ID #{id}</span> last updated by{' '}
@@ -36,7 +36,8 @@ const LogItem = ({ log, deleteLog }) => {
 
 LogItem.propTypes = {
     log: PropTypes.object.isRequired,
-    deleteLog: PropTypes.func.isRequired
+    deleteLog: PropTypes.func.isRequired,
+    setCurrent: PropTypes.func.isRequired
 }
 
-export default connect(null, { deleteLog })(LogItem);
+export default connect(null, { deleteLog, setCurrent })(LogItem);
